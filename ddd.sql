bcc0cb86ca6830bdd15d065fa5b00ce51c4c0b00
shadiqurit <shadiqur.it@gmail.com>
2024-11-25 15:50:54 +0600

    ddd

--- ddd.sql
+++ ddd.sql
@@ -1,9 +1,12 @@
-WITH Days AS (
-  SELECT TO_DATE('01-JAN-2023', 'DD-MON-YYYY') + (LEVEL - 1) AS DayDate
-  FROM DUAL
-  CONNECT BY LEVEL <= 365
-)
-SELECT 
-  TO_CHAR(DayDate, 'Day') AS DayName,
-  TO_CHAR(DayDate, 'D') AS DayNumber
-FROM Days;
+/* Formatted on 11/25/2024 8:08:07 AM (QP5 v5.362) */
+WITH
+    Days
+    AS
+        (    SELECT TO_DATE ('01-JAN-2024', 'DD-MON-YYYY') + (LEVEL - 1)    AS DayDate
+               FROM DUAL
+         CONNECT BY LEVEL <= 1)
+SELECT ROWNUM                               nthday,
+       TO_CHAR (DayDate, 'DD-MON-YYYY')     dateinyear,
+       TO_CHAR (DayDate, 'Day')             AS DayName,
+       TO_CHAR (DayDate, 'D')               AS DayNumber
+  FROM Days;
