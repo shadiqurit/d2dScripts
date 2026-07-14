3df7d7e6f6d4e48471212de4f576368743edf964
Shadiq <shadiq1297@gmail.com>
2026-07-14 14:04:40 +0600

    Update data using HRMS and IPIHR scripts

---
+++ HRMS/actual_emp_info.sql
@@ -0,0 +1,13 @@
+CREATE TABLE empinfo_update (
+    empcode         VARCHAR2(50),
+    Empno           VARCHAR2(50),
+    Employeename    VARCHAR2(100),
+    Businessunit    VARCHAR2(30),
+    GRADE           VARCHAR2(30),
+    Costcenter      VARCHAR2(100),
+    Designation     VARCHAR2(100),
+    Department      VARCHAR2(100),
+    Paymenttype     VARCHAR2(100),
+    Paysliplocation VARCHAR2(100),
+    LocationName    VARCHAR2(100)
+);
